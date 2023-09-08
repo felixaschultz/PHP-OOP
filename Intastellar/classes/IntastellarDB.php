b1afa316497f83895f5f9bdccb90e41481247897
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-08 11:23:40 +0200

    Added some methods

--- Intastellar/classes/IntastellarDB.php
+++ Intastellar/classes/IntastellarDB.php
@@ -1,16 +1,29 @@
 <?php
     class IntastellarDB extends Intastellar{
         public function __construct($var = null) {
-            $this->host = 'localhost';
-            $this->user = 'localhost';
-            $this->pass = 'localhost';
-            $this->db = 'localhost';
+            $this->$host = 'localhost';
+            $this->$user = 'localhost';
+            $this->$pass = 'localhost';
+            $this->$db = 'localhost';
         }
 
         public function connect(){
-            $this->db = new mysqli($this->host, $this->user, $this->pass, $this->db);
-            if($this->db->connect_errno > 0){
-                die('Unable to connect to database [' . $this->db->connect_error . ']');
+            $this->$db = new mysqli($this->$host, $this->$user, $this->$pass, $this->$db);
+            if($this->$db->connect_errno > 0){
+                die('Unable to connect to database [' . $this->$db->connect_error . ']');
             }
+        }
+
+        public function disconnect(){
+            $this->$db->close();
+        }
+
+        public function query($query){
+            $this->query = $query;
+            $this->result = $this->db->query($this->query);
+        }
+
+        public function fetch(){
+            $this->fetch = $this->result->fetch_object();
         }
     }
