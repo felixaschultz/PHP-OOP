545392e02228f351f0eeae3fe270db9acb6f80d5
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-08 11:04:47 +0200

    Added constructors

--- IntastellarDB.php
+++ IntastellarDB.php
@@ -1,6 +1,16 @@
 <?php
     class IntastellarDB extends Intastellar{
         public function __construct($var = null) {
-            $this->$var = $var;
+            $this->host = 'localhost';
+            $this->user = 'localhost';
+            $this->pass = 'localhost';
+            $this->db = 'localhost';
+        }
+
+        public function connect(){
+            $this->db = new mysqli($this->host, $this->user, $this->pass, $this->db);
+            if($this->db->connect_errno > 0){
+                die('Unable to connect to database [' . $this->db->connect_error . ']');
+            }
         }
     }
--- IntastellarUser.class.php
+++ IntastellarUser.class.php
@@ -1,5 +1,7 @@
 <?php
     
     class IntastellarAccounts extends Intastellar{
-        
+        public function __construct($var = null) {
+            $this->$var = $var;
+        }
     }
