7cb2df50dc80ba80bccd8bf40e6c8c42b88b3a22
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-08 11:23:24 +0200

    Created some default methods

--- Intastellar/classes/IntastellarLogin.class.php
+++ Intastellar/classes/IntastellarLogin.class.php
@@ -4,4 +4,38 @@
         public function __construct($var = null) {
             $this->$var = $var;
         }
+
+        public function login($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+            $this->login = true;
+        }
+
+        public function logout(){
+            $this->login = false;
+        }
+
+        public function register($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+            $this->register = true;
+        }
+
+        public function delete($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+            $this->delete = true;
+        }
+
+        public function update($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+            $this->update = true;
+        }
+
+        public function get($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+            $this->get = true;
+        }
     }
