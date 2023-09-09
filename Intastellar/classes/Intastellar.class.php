a5fb071f8df9500ada0ad1f72cf668f3f8a7176d
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-09 11:51:57 +0200

    Created DB Class

--- Intastellar/classes/Intastellar.class.php
+++ Intastellar/classes/Intastellar.class.php
@@ -1,44 +1,140 @@
 <?php
     namespace Intastellar;
-    class Intastellar {
+    include_once("db.class.php");
+    /**
+     * Summary of Account
+     * @property mixed $username
+     * @property mixed $password
+     * @property mixed $email
+     * @property mixed $id
+     * @property mixed $created
+     * @property mixed $last_login
+     * @property mixed $last_logout
+     * @property mixed $status
+     * @property mixed $role
+     */
+    class Account {
+        /**
+         * Summary of __construct
+         */
+        var $username;
+        var $password;
+        var $email;
+        var $id;
+        var $created;
+        var $last_login;
+        var $last_logout;
+        var $status;
+        var $role;
+        var $db = new \Intastellar\DB();
         public function __construct() {
             
         }
 
         // Login an Intastellar User
+        /**
+         * Summary of userLogin
+         * @param mixed $username
+         * @param mixed $password
+         * @return void
+         */
         function userLogin($username, $password){
             
         }
 
         // Log an Intastellar User out
+        /**
+         * Summary of userLogout
+         * @param mixed $username
+         * @return void
+         */
         function userLogout($username){
 
         }
 
         // Create a new Intastellar User
+        /**
+         * Summary of userCreate
+         * @return void
+         */
         function userCreate(){
 
         }
 
         // Delete an Intastellar User
+        /**
+         * Summary of userDelete
+         * @param mixed $id
+         * @return void
+         */
         function userDelete($id){
 
         }
 
         // Update an Intastellar User
+        /**
+         * Summary of userUpdate
+         * @param mixed $id
+         * @return void
+         */
         function userUpdate($id){
 
         }
 
         // Get an Intastellar User
+        /**
+         * Summary of userGet
+         * @param mixed $id
+         * @return void
+         */
         function userGet($id){
 
         }
 
         // Get all Intastellar Users
+        /**
+         * Summary of userGetAll
+         * @return void
+         */
         function userGetAll(){
 
         }
 
+        // Get all Intastellar Users by Role
+        /**
+         * Summary of userGetAllByRole
+         * @param mixed $role
+         * @return void
+         */
+        function userGetAllByRole($role){
+
+        }
+
+        // Get all Intastellar Users by Status
+        /**
+         * Summary of userGetAllByStatus
+         * @param mixed $status
+         * @return void
+         */
+        function userGetAllByStatus($status){
+
+        }
+
+        // Get all Intastellar Users by Role and Status
+        /**
+         * Summary of userGetAllByRoleAndStatus
+         * @param mixed $role
+         * @param mixed $status
+         * @return void
+         */
+        function userGetAllByRoleAndStatus($role, $status){
+        
+        }
         
     }
+
+    $IntastellarAccount = new \Intastellar\Account();
+
+    echo $IntastellarAccount->userLogin("test", "test");
+
+?>
---
+++ Intastellar/classes/db.class.php
@@ -0,0 +1,83 @@
+<?php
+    namespace Intastellar;
+    /**
+     * Summary of DB
+     * @property mixed $username
+     * @property mixed $password
+     * @property mixed $email
+     */
+
+    class DB {
+        /**
+         * Summary of __construct
+         */
+        public function __construct() {
+            
+        }
+
+        // Connect to the Intastellar Database
+        /**
+         * Summary of connect
+         * @return void
+         */
+        function connect(){
+
+        }
+
+        // Disconnect from the Intastellar Database
+        /**
+         * Summary of disconnect
+         * @return void
+         */
+        function disconnect(){
+
+        }
+
+        // Execute a query on the Intastellar Database
+        /**
+         * Summary of query
+         * @param mixed $query
+         * @return void
+         */
+        function query($query){
+
+        }
+
+        // Execute a query on the Intastellar Database and return the result
+        /**
+         * Summary of queryResult
+         * @param mixed $query
+         * @return void
+         */
+        function queryResult($query){
+
+        }
+
+        // Execute a query on the Intastellar Database and return the number of rows affected
+        /**
+         * Summary of queryAffected
+         * @param mixed $query
+         * @return void
+         */
+        function queryAffected($query){
+
+        }
+
+        // Execute a query on the Intastellar Database and return the number of rows affected
+        /**
+         * Summary of queryLastInsertId
+         * @param mixed $query
+         * @return void
+         */
+        function queryLastInsertId($query){
+
+        }
+
+        // Execute a query on the Intastellar Database and return the number of rows affected
+        /**
+         * Summary of queryLastInsertId
+         * @param mixed $query
+         * @return void
+         */
+    }
+?>
